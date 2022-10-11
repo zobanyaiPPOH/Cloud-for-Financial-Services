9087d9de8cba371b094b0a2bf3be10b3685cf082
zobanyaiPPOH <114498761+zobanyaiPPOH@users.noreply.github.com>
2022-10-11 13:20:53 +0200

    Update delegator.ts

--- intelligent-appointment/demo-web-app/src/helpers/delegator.ts
+++ intelligent-appointment/demo-web-app/src/helpers/delegator.ts
@@ -5,7 +5,7 @@ import { IRequestPayload } from "@fsi/appointment-starter-kit";
 import { sendRequest } from "./httpPortal.helper";
 
 //Your gateway endpoint URL, here it's the serverless function endpoint example
-const endpoint = 'api/FSIConnect';
+const endpoint = '/api/FSIConnect';
 
 export const delegator = async (request: IRequestPayload) => {   
     const result = await sendRequest(endpoint, {
